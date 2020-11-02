e30e0edd721b5b2474ecafd799634d3fd324e1b7
hshaosf <11635576+hshaosf@users.noreply.github.com>
2020-11-01 23:20:28 -0800

    send submission to accela

--- service/resources/submission.py
+++ service/resources/submission.py
@@ -3,19 +3,23 @@
 import os
 import sys
 import json
+import datetime
 import falcon
 import jsend
 import sentry_sdk
+import requests
 from airtable import Airtable
 from .hooks import validate_access
+from ..modules.util import timer
 from ..modules.formio import Formio
 from ..transforms.submission_transform import SubmissionTransform
+
 
 @falcon.before(validate_access)
 class Submission():
     """Submission class"""
     def on_post(self, req, resp):
-        #pylint: disable=no-self-use
+        #pylint: disable=no-self-use,too-many-locals
         """
             on post request
         """
@@ -33,35 +37,43 @@ class Submission():
                 accela_prj_id = "" # placeholder accela_prj variable
                 accela_sys_id = "" # placeholder accela_sys_id variable
 
-                submission_json = Formio.get_formio_submission_by_id(
-                    submission_id, form_id=os.environ.get('FORMIO_FORM_ID_ADU'))
-                msg = submission_json
+                submission_json = self.get_submssion_json(submission_id)
 
                 # init airtable
                 airtable = self.get_airtable()
-                airtable.insert({
-                    'FORMIO_ID': submission_id,
-                    'SUBMISSION_DATE': submission_json['created'],
-                    'PROJECT_ADDRESS': submission_json['data']['projectAddress'],
-                    'FIRST_NAME': submission_json['data']['firstName'],
-                    'LAST_NAME': submission_json['data']['lastName'],
-                    'EMAIL': submission_json['data']['email']
-                })
-                msg = {'airtable': airtable.get_all()}
+                # log submission
+                insert = self.create_submission_airtable(airtable, submission_id, submission_json)
+                airtable_id = insert["id"]
 
                 # transform submission into record
                 record_json = SubmissionTransform().transform(submission_json)
-                msg = record_json
 
-                resp.body = json.dumps(jsend.success(msg))
-                resp.status = falcon.HTTP_200
-                sentry_sdk.capture_message(
-                    'ADU Intake {submission_id} {accela_prj_id} {accela_sys_id}'.format(
-                        submission_id=submission_id,
-                        accela_prj_id=accela_prj_id,
-                        accela_sys_id=accela_sys_id
-                    ), 'info')
-                return
+                # send record to accela
+                response = self.send_record_to_accela(record_json)
+
+                if response.status_code == 200:
+                    content_json = response.json()
+
+                    self.update_submission_airtable(airtable, airtable_id, content_json)
+
+                    msg = content_json
+
+                    resp.body = json.dumps(jsend.success(msg))
+                    resp.status = falcon.HTTP_200
+                    #pylint: disable=line-too-long
+                    sentry_sdk.capture_message(
+                        'ADU Intake {submission_id} {accela_env} {accela_prj_id} {accela_sys_id}'.format(
+                            submission_id=submission_id,
+                            accela_prj_id=accela_prj_id,
+                            accela_sys_id=accela_sys_id,
+                            accela_env=os.environ.get('ACCELA_ENV')
+                        ), 'info')
+                    return
+
+                with sentry_sdk.configure_scope() as scope:
+                    scope.set_extra('accela_response_status_code', response.status_code)
+                    scope.set_extra('accela_response_json', response.json())
+
 
         # catch-all
         resp.status = falcon.HTTP_400
@@ -71,9 +83,60 @@ class Submission():
         return
 
     @staticmethod
+    @timer
     def get_airtable():
         """ Get airtable """
         return Airtable(
             os.environ.get('AIRTABLE_SUBMISSION_BASE_KEY'),
             os.environ.get('AIRTABLE_SUBMISSION_TABLE_NAME'),
             os.environ.get('AIRTABLE_API_KEY'))
+
+    @staticmethod
+    @timer
+    def create_submission_airtable(airtable, submission_id, submission_json):
+        """ Create submission into AirTable """
+        return airtable.insert({
+                    'FORMIO_ID': submission_id,
+                    'SUBMISSION_DATE': submission_json['created'],
+                    'PROJECT_ADDRESS': submission_json['data']['projectAddress'],
+                    'FIRST_NAME': submission_json['data']['firstName'],
+                    'LAST_NAME': submission_json['data']['lastName'],
+                    'EMAIL': submission_json['data']['email'],
+                    'ACCELA_ENV': os.environ.get('ACCELA_ENV')
+                })
+
+    @staticmethod
+    @timer
+    def update_submission_airtable(airtable, airtable_id, accela_json):
+        """ Update submission into Airtable """
+        fields = {
+            'ACCELA_PRJ_ID': accela_json['result']['customId'],
+            'ACCELA_SYS_ID': accela_json['result']['id'],
+            'ACCELA_CREATED_DATE': datetime.datetime.now(datetime.timezone.utc).isoformat()
+        }
+        update = airtable.update(airtable_id, fields)
+        return update
+
+    @staticmethod
+    @timer
+    def get_submssion_json(submission_id):
+        """ Get Submission JSON """
+        submission_json = Formio.get_formio_submission_by_id(
+                    submission_id, form_id=os.environ.get('FORMIO_FORM_ID_ADU'))
+        return submission_json
+
+    @staticmethod
+    @timer
+    def send_record_to_accela(record_json):
+        """ Send record to Accela """
+        url = os.environ.get('ACCELA_MS_BASE_URL') + '/records'
+        headers = {
+            'X-SFDS-APIKEY': os.environ.get('ACCELA_MS_APIKEY'),
+            'X-ACCELA-ENV': os.environ.get('ACCELA_ENV'),
+            'X-ACCELA-USERNAME': os.environ.get('ACCELA_USERNAME')
+        }
+        params = {}
+        data = json.dumps(record_json)
+        response = requests.post(url, headers=headers, data=data, params=params)
+
+        return response
