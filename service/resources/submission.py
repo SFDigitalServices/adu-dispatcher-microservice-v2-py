7219589817b8165316c4b887e2a2bc8364f91fae
hshaosf <11635576+hshaosf@users.noreply.github.com>
2020-11-01 16:08:22 -0800

    setup airtable and sentry log

--- service/resources/submission.py
+++ service/resources/submission.py
@@ -5,6 +5,8 @@ import sys
 import json
 import falcon
 import jsend
+import sentry_sdk
+from airtable import Airtable
 from .hooks import validate_access
 from ..modules.formio import Formio
 
@@ -21,18 +23,42 @@ class Submission():
             data_json = json.loads(data)
             msg = data_json
 
+            with sentry_sdk.configure_scope() as scope:
+                scope.set_extra('data_json', data_json)
+
             if 'id' in data_json:
+                # get submission json
                 submission_id = data_json['id']
                 submission_json = Formio.get_formio_submission_by_id(
                     submission_id, form_id=os.environ.get('FORMIO_FORM_ID_ADU'))
                 msg = submission_json
 
+                # init airtable
+                airtable = self.get_airtable()
+                airtable.insert({
+                    'FORMIO_ID': submission_id,
+                    'SUBMISSION_DATE':submission_json['created']})
+                msg = {'airtable': airtable.get_all()}
+
+                # transform submission
+
                 resp.body = json.dumps(jsend.success(msg))
                 resp.status = falcon.HTTP_200
+                sentry_sdk.capture_message(
+                    'ADU Intake {submission_id}'.format(submission_id=submission_id), 'info')
                 return
 
         # catch-all
         resp.status = falcon.HTTP_400
         msg = "The create record information is missing"
         resp.body = json.dumps(jsend.error(msg))
+        sentry_sdk.capture_message('ADU Inake Error', 'error')
         return
+
+    @staticmethod
+    def get_airtable():
+        """ Get airtable """
+        return Airtable(
+            os.environ.get('AIRTABLE_SUBMISSION_BASE_KEY'),
+            os.environ.get('AIRTABLE_SUBMISSION_TABLE_NAME'),
+            os.environ.get('AIRTABLE_API_KEY'))
