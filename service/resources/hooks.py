f0890cae4a8d6509483029597b1aa5a521f5ccce
hshaosf <11635576+hshaosf@users.noreply.github.com>
2021-03-03 13:14:37 -0800

    update access checks for more flexible local testing

--- service/resources/hooks.py
+++ service/resources/hooks.py
@@ -5,5 +5,9 @@ import falcon
 def validate_access(req, _resp, _resource, _params):
     """ validate access method """
     access_key = os.environ.get('ACCESS_KEY')
-    if not access_key or req.get_header('ACCESS_KEY') != access_key:
+    if not access_key or \
+        (
+                req.get_header('ACCESS_KEY') != access_key and
+                not (req.host == '127.0.0.1' and req.get_header('x-apikey') == access_key)
+        ):
         raise falcon.HTTPForbidden(description='Access Denied')
